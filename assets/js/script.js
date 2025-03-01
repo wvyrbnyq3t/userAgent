ad5eb919fce7be2428211f455956143730dcdde5
SotaDesktop <kansota1125@gmail.com>
2025-03-01 16:37:11 +0900

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
-const btnGetUserAgent = document.getElementById('btnGetUserAgent');
-btnGetUserAgent.addEventListener("click", () => {
-  alert(window.navigator.userAgent.toLowerCase());
-})
+// カルーセルの子要素の個数
+document.addEventListener("DOMContentLoaded", () => {
+  const carousels = document.querySelectorAll(".carousel");
+  carousels.forEach((carousel) => {
+    // 子要素(.carousel-item)の個数
+    const children = carousel.querySelectorAll(".carousel-item").length;
+    carousel.setAttribute("style", `--i: ${children}`);
+  });
+});
